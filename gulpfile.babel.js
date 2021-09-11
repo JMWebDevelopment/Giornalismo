5bfead94d27d2950c5ff20664f2809d2066c5488
Jacob Martella <me@jacobmartella.com>
2021-09-10 20:49:21 -0500

    Added in the font awesome icons.
    Addresses #2 #5

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -18,13 +18,14 @@ import watch from './gulp/watch';
 import prodPrep from './gulp/prodPrep';
 import prodStringReplace from './gulp/prodStringReplace';
 import prodCompress from './gulp/prodCompress';
+import webfonts from './gulp/webfonts';
 import {cleanCSS, cleanJS} from './gulp/clean';
 
 /**
  * Map out the sequence of events on first load and make it the default task
  */
 export const firstRun = series(
-    cleanCSS, cleanJS, parallel(php, images, series( styles, editorStyles ), scripts), serve, watch
+    cleanCSS, cleanJS, parallel(php, images, series( styles, editorStyles ), scripts), webfonts, serve, watch
 );
 
 export default firstRun;
@@ -33,17 +34,17 @@ export default firstRun;
  * Build theme for development without BrowserSync or watching
  */
 export const buildDev = parallel(
-    php, images, series( styles, editorStyles ), scripts, translate
+    php, images, series( styles, editorStyles ), scripts, webfonts, translate
 );
 
 /**
  * Export theme for distribution.
  */
 export const bundleTheme = series(
-    prodPrep, parallel(php, scripts, series( styles, editorStyles ), images), translate, prodStringReplace, prodCompress
+    prodPrep, parallel(php, scripts, series( styles, editorStyles ), images), webfonts, translate, prodStringReplace, prodCompress
 );
 
 /**
  * Export all imported functions as tasks
  */
-export { generateCert, images, php, scripts, styles, editorStyles, translate, watch, cleanCSS, cleanJS };
+export { generateCert, images, php, scripts, styles, editorStyles, translate, webfonts, watch, cleanCSS, cleanJS };
